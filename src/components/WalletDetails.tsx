c950f35cc59cb6a7f5189e671081eaca995ccc9f
yuripaoloni <yuri.paoloni3@gmail.com>
2021-11-13 14:59:03 +0100

    feat: smart contract implementation

--- src/components/WalletDetails.tsx
+++ src/components/WalletDetails.tsx
@@ -1,14 +1,19 @@
 import { useWeb3React } from "@web3-react/core";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
+import { contracts } from "config/contracts";
+import hardhatTestABI from "config/abi/hardhatTest.json";
 
 const WalletDetails = () => {
   const { account, library } = useWeb3React();
 
   const [balance, setBalance] = useState("");
   const [blockNumber, setBlockNumber] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchBalance = async () => {
@@ -29,8 +34,33 @@ const WalletDetails = () => {
     setBlockNumber(blockNumber);
   };
 
+  const contractInteraction = async () => {
+    const contract = new ethers.Contract(
+      contracts.hardhatTest,
+      hardhatTestABI,
+      library
+    );
+    const signer = library.getSigner();
+
+    const contractSigner = contract.connect(signer);
+
+    setLoading(true);
+    await contractSigner.transfer(account, 20);
+
+    const balance = await contractSigner.owner();
+    console.log(ethers.utils.formatEther(balance));
+
+    setLoading(false);
+  };
+
   return account ? (
     <div>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <Button
         sx={{ mt: 4 }}
         variant="contained"
@@ -45,6 +75,14 @@ const WalletDetails = () => {
         </Typography>
       )}
       <Typography sx={{ mt: 2 }}>Balance {balance} ETH</Typography>
+      <Button
+        sx={{ mt: 4 }}
+        variant="contained"
+        color="secondary"
+        onClick={() => contractInteraction()}
+      >
+        CONNECT CONTRACT
+      </Button>
     </div>
   ) : null;
 };
---
+++ src/config/contracts.ts
@@ -0,0 +1,3 @@
+export const contracts = {
+  hardhatTest: "0x1fC9399d98bD83e380b38d60d3aca39B3c4b3D87",
+};
